44800a93b42fc691b1793f5a7f95ce8fd916e76c
Markus Scheidgen <markus.scheidgen@gmail.com>
2025-10-02 22:25:19 +0200

    Added lecture text markdown rendering.

--- src/components/LectureText.tsx
+++ src/components/LectureText.tsx
@@ -1,12 +1,20 @@
-import {Typography} from '@mui/joy'
-import React from 'react'
+import {Typography} from "@mui/joy"
+import React from "react"
+import ReactMarkdown from "react-markdown"
+import "../styles/markdown.css" // New import
 
 type LectureTextProps = {
-  children: React.ReactNode
+  children: string // Children should be a string for markdown
 }
 
 function LectureText({children}: LectureTextProps) {
-  return <Typography>{children}</Typography>
+  return (
+    <Typography component="div" className="markdown-body">
+      {" "}
+      {/* Add className */}
+      <ReactMarkdown>{children}</ReactMarkdown>
+    </Typography>
+  )
 }
 
 export default LectureText
